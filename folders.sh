ebe5294438df1add025bea3aa93808929eb0a397
age-killer <archimed@dir.bg>
2016-10-21 11:46:11 +0300

    Add files via upload

---
+++ folders.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p /home/$username/workfolder/OutputInvoices
+mkdir -p /home/$username/workfolder/OutputInvoices/e5FU
+mkdir -p /home/$username/workfolder/OutputInvoices/e5SE
+mkdir -p /home/$username/workfolder/OutputInvoices/e5IT
+mkdir -p /home/$username/workfolder/OutputInvoices/e5MA
+mkdir -p /home/$username/workfolder/OutputInvoices/e5SH
+mkdir -p /home/$username/workfolder/OutputInvoices/e5UT
+mkdir -p /home/$username/workfolder/OutputInvoices/e5OT
+mkdir -p /home/$username/workfolder/OutputInvoices/e5AS
+
+cp instanceV6_MIFF.sh ~/workfolder
+cp variables.incl ~/workfolder
